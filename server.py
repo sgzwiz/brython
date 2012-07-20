657a9354663d1789feb5c62e751bf13a1a2e3b49
pierre.quentel <pierre.quentel@gmail.com>
2012-07-20 10:04:57 +0000

    Rewriting of drag and drop implementation and test

--- server.py
+++ server.py
@@ -3,4 +3,5 @@ server_address = ('', 8000)
 handler = http.server.CGIHTTPRequestHandler
 handler.cgi_directories = ['/test/cgi-bin']
 httpd = http.server.HTTPServer(server_address, handler)
+print("server running on port 8000")
 httpd.serve_forever()
