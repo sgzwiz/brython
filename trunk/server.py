9e359fa634fe01930dc3a6f762128ea60bd09e26
pierre.quentel@gmail.com <pierre.quentel@gmail.com@570a7cb1-34e0-a994-6e4f-44535eead511>
2012-08-28 09:40:22 +0000

    Change server.py and ajax.html because of cgi-bin folder change

--- trunk/server.py
+++ trunk/server.py
@@ -1,7 +1,6 @@
 import http.server
 server_address = ('', 8000)
 handler = http.server.CGIHTTPRequestHandler
-handler.cgi_directories = ['/test/cgi-bin']
 httpd = http.server.HTTPServer(server_address, handler)
 print("server running on port 8000")
 httpd.serve_forever()
