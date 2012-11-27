278c216b2c14ca70296d1be37c04a71148ac11a0
pierre.quentel@gmail.com <pierre.quentel@gmail.com@570a7cb1-34e0-a994-6e4f-44535eead511>
2012-11-27 17:36:03 +0000

    math.js : explicit message if attribute not found

--- trunk/math.js
+++ trunk/math.js
@@ -1,5 +1,9 @@
 math = {
-    __getattr__ : function(attr){return this[attr]},
+    __getattr__ : function(attr){
+        var res = this[attr]
+        if(res===undefined){$raise('AttributeError','module has no attribute '+attr)}
+        return res
+    },
     cos : function(x){return float(Math.cos(x.value))},
     floor:function(x){return int(Math.floor(x.value))},
     pi : float(Math.PI),
