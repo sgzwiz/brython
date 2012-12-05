74a7cbe84cdff21e37d16237fb183c02be76f637
pierre.quentel@gmail.com <pierre.quentel@gmail.com@570a7cb1-34e0-a994-6e4f-44535eead511>
2012-12-05 18:24:43 +0000

    Demo updates

--- trunk/test/test_suite.py
+++ trunk/test/test_suite.py
@@ -1,36 +1,18 @@
-def foo(x):
- return x.upper()
-alert(max(['a','Z'],key=foo))
-alert(max(['A','Z']))
-t = ['a','b']
-t[0]+='bsdfqs'
-alert(t)
-alert('ab'*3)
-z = 'a'
-z += 'bc'
-alert('Z'+z)
 
-x={1:'a',2:'b'}
-for k,v in x.items():
- alert('%s:%s' %(k,v))
-
-x='azerty'
-v = ''
-for u in reversed(x):
- v += u
-alert(v)
-
-'azertu'.endswith(('a','tu'))
-'azertu'.startswith(('a','sazer'))
-'15'.isdigit()
-',,'.join(['a','bc'])
-
-x = {1:2,3:4}
-log(x)
-log(x[1])
-del x[1]
-log(x)
-
-x = [1,2,5,8,99,'a']
-del x[1::2]
-log(x)
+# list examples
+a=['spam','eggs',100,1234]
+log(a[:2]+['bacon',2*2])
+log(3*a[:3]+['Boo!'])
+log(a[:])
+a[2]=a[2]+23
+log(a)
+a[0:2]=[1,12]
+log(a)
+a[0:2]=[]
+log(a)
+a[1:1]=['bletch','xyzzy']
+log(a)
+a[:0]=a
+log(a)
+a[:]=[]
+log(a)
