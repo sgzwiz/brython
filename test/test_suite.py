f789c6dbef6e2dca37fce157d65f5dcf7424f612
pierre.quentel <pierre.quentel@gmail.com>
2012-12-08 12:54:56 +0000

    Demo updates

--- test/test_suite.py
+++ test/test_suite.py
@@ -1,3 +1,50 @@
+# numbers
+log(2+2)
+log((50-5*6)/4)
+log(8/5)
+log(7//3)
+log(7//-3)
+width=20
+height=5*9
+log(width*height)
+x=y=z=0
+log(x)
+log(y)
+log(z)
+try:
+  log(n)
+except:
+  log('n is not defined')
+log(3 * 3.75 / 1.5)
+log(7.0 / 2)
+
+# strings
+log('spam eggs')
+log('doesn\'t')
+log("doesn't")
+log('"Yes," he said.')
+log("\"Yes,\" he said.")
+log('"Isn\'t," she said.')
+hello = "This is a rather long string containing\n\
+several lines of text just as you would do in C.\n\
+    Note that whitespace at the beginning of the line is\
+ significant."
+log(hello)
+log("""\
+Usage: thingy [OPTIONS]
+     -h                        Display this usage message
+     -H hostname               Hostname to connect to
+""")
+hello = r"This is a rather long string containing\n\
+several lines of text much as you would do in C."
+log(hello)
+
+word = 'Help' + 'A'
+log(word)
+log('<' + word*5 + '>')
+
+log('str' 'ing')
+log('str'.strip() + 'ing')
 
 # list examples
 a=['spam','eggs',100,1234]
