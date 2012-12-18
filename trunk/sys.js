e8445c5a4a0aa3c5c9707bef99e7e759ff622f97
pierre.quentel@gmail.com <pierre.quentel@gmail.com@570a7cb1-34e0-a994-6e4f-44535eead511>
2012-12-18 21:27:40 +0000

    Latest version of sys.js and brython.js

--- trunk/sys.js
+++ trunk/sys.js
@@ -3,5 +3,5 @@ sys = {
         if(attr in this){return this[attr]}
         else{$raise('AttributeError','module sys has no attribute '+attr)}},
     has_local_storage:typeof(Storage)!=="undefined",
-    version_info:[1,0,"20121218-203137"]
+    version_info:[1,0,"20121218-222709"]
 }
