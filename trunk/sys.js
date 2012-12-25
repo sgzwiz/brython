d1d7123ebc4f585ff1eea214029e64d0f787b240
pierre.quentel@gmail.com <pierre.quentel@gmail.com@570a7cb1-34e0-a994-6e4f-44535eead511>
2012-12-25 16:17:51 +0000

    sys.js : allow definition of sys.stderr and sys.stdout

--- trunk/sys.js
+++ trunk/sys.js
@@ -1,7 +1,13 @@
 sys = {
     __getattr__ : function(attr){
-        if(attr in this){return this[attr]}
+        if(attr==="stdout"){return $stdout}
+        if(attr==="stderr"){return $stderr}
+        else if(attr in this){return this[attr]}
         else{$raise('AttributeError','module sys has no attribute '+attr)}},
+    __setattr__ : function(attr,value){
+        if(attr==="stdout"){$stdout=value}
+        if(attr==="stderr"){$stderr=value}
+    },
     has_local_storage:typeof(Storage)!=="undefined",
-    version_info:[1,0,"20121223-185037"]
+    version_info:[1,0,"20121225-170510"]
 }
