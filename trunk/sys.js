5ac9af5575605e5b07e7b33c6f6f7ed0dca22e65
pierre.quentel@gmail.com <pierre.quentel@gmail.com@570a7cb1-34e0-a994-6e4f-44535eead511>
2012-12-28 07:55:55 +0000

    sys.js : replace $stdout by document.$stdout

--- trunk/sys.js
+++ trunk/sys.js
@@ -1,13 +1,13 @@
 sys = {
     __getattr__ : function(attr){
-        if(attr==="stdout"){return $stdout}
-        if(attr==="stderr"){return $stderr}
+        if(attr==="stdout"){return document.$stdout}
+        if(attr==="stderr"){return document.$stderr}
         else if(attr in this){return this[attr]}
         else{$raise('AttributeError','module sys has no attribute '+attr)}},
     __setattr__ : function(attr,value){
-        if(attr==="stdout"){$stdout=value}
-        if(attr==="stderr"){$stderr=value}
+        if(attr==="stdout"){document.$stdout=value}
+        if(attr==="stderr"){document.$stderr=value}
     },
     has_local_storage:typeof(Storage)!=="undefined",
-    version_info:[1,0,"20121225-174458"]
+    version_info:[1,0,"20121228-085057"]
 }
