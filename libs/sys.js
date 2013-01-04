46b52fd87898977240469868fcabc70955142efe
pierre.quentel <pierre.quentel@gmail.com>
2013-01-04 07:30:14 +0000

    Major site reorganisation to allow use with local files instead of through an HTTP server : put built-in JS modules in subfolder /libs ; replace all links by relative links ; modify $import accordingly, using the path of brython.js

--- libs/sys.js
+++ libs/sys.js
@@ -9,5 +9,5 @@ sys = {
         if(attr==="stderr"){document.$stderr=value}
     },
     has_local_storage:typeof(Storage)!=="undefined",
-    version_info:[1,0,"20130101-221546"]
+    version_info:[1,0,"20130104-080027"]
 }
