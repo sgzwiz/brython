dbe93d4af1db8964984d4d4e1f06a5012684ca49
pierre.quentel <pierre.quentel@570a7cb1-34e0-a994-6e4f-44535eead511>
2013-01-06 08:03:52 +0000

    py_tokenizer.js minor cleanup

--- py_tokenizer.js
+++ py_tokenizer.js
@@ -8,7 +8,6 @@ var $operators = {
     "^":"pow","<":"lt",">":"gt",
     "<=":"le",">=":"ge","==":"eq","!=":"ne",
     "or":"or","and":"and","in":"in","not":"not",
-    "in":"in","not":"not",
     "not_in":"not_in","is_not":"is_not" // fake
     }
 
