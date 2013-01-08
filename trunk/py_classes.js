f91cab8e8aa1f34b1e1338e3d3982c6975f8c60d
pierre.quentel@gmail.com <pierre.quentel@gmail.com@570a7cb1-34e0-a994-6e4f-44535eead511>
2013-01-08 08:06:19 +0000

    py_classes.js : int() sets class of returned value to int, which is no more the __class__ of Number

--- trunk/py_classes.js
+++ trunk/py_classes.js
@@ -437,18 +437,20 @@ function $import(){
 function int(value){
     if(typeof value=="number" ||
         (typeof value=="string" && parseInt(value)!=NaN)){
-        return parseInt(value)
+        var res = new Number(parseInt(value))
+        res.__class__ = int
+        console.log('create int '+value+' '+res.__class__)
+        return res
     }else if(isinstance(value,float)){
-        return parseInt(value.value)
+        var res = new Number(parseInt(value.value))
+        res.__class__ = int
+        return res
     }else{ $raise('ValueError',
         "Invalid literal for int() with base 10: '"+str(value)+"'")
     }
 }
 int.__class__ = $type
 int.toString = function(){return "<class 'int'>"}
-
-// Python integers are Javascript numbers
-Number.prototype.__class__ = int
 
 Number.prototype.__floordiv__ = function(other){
     if(isinstance(other,int)){
