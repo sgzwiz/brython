4c4bcc682cb8b00a16f02c9218f3eec07e7e3438
pierre.quentel <pierre.quentel@gmail.com>
2013-01-23 21:55:49 +0000

    Latest version of brython.js and sys.js

--- libs/sys.js
+++ libs/sys.js
@@ -9,5 +9,5 @@ $module = {
         if(attr==="stderr"){document.$stderr=value}
         },
     has_local_storage:typeof(Storage)!=="undefined",
-    version_info:[1,0,"20130122-195420"]
+    version_info:[1,0,"20130123-223708"]
 }
