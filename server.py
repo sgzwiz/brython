e16e22e53b483a47cba7d9d6ff72a2847647a9ca
pierre.quentel <pierre.quentel@gmail.com>
2013-02-02 18:06:35 +0000

    issue 63 : server.py works on Python 2 and 3 ; remove server_python2.py and server_python3.py (written by Billy Earney)

--- server.py
+++ server.py
@@ -1,6 +1,16 @@
-import http.server
-server_address = ('', 8001)
-handler = http.server.CGIHTTPRequestHandler
-httpd = http.server.HTTPServer(server_address, handler)
+import sys
+
+if sys.version < '3':
+   import BaseHTTPServer as server
+   from CGIHTTPServer import CGIHTTPRequestHandler
+else:
+   import http.server as server
+   from http.server import CGIHTTPRequestHandler
+
+server_address = ('', 8000)
+handler = CGIHTTPRequestHandler
+httpd = server.HTTPServer(server_address, handler)
 print("server running on port %s" %server_address[1])
 httpd.serve_forever()
+
+
--- server_python2.py
+++
@@ -1,6 +0,0 @@
-import CGIHTTPServer,BaseHTTPServer
-server_address = ('', 8000)
-handler = CGIHTTPServer.CGIHTTPRequestHandler
-httpd = BaseHTTPServer.HTTPServer(server_address, handler)
-print("server running on port %s" %server_address[1])
-httpd.serve_forever()
