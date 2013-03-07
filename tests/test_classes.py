7b5c797763ef04c9c2f1cdb3f5d447efad1eddfd
pierre.quentel <pierre.quentel@gmail.com>
2013-03-07 16:41:23 +0000

    More tests for class inheritance

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -7,19 +7,24 @@ class bar(baz):
   def test1(self,x):
     return x*'test1'
 
-class foo(bar):
+class truc:
+    machin = 99
+    
+class foo(bar,truc):
  def test(self):
   return 'test in foo'
  def test2(self):
    return 'test2'
 
 obj = foo()
-print(bar.test)
-print(obj.test)
+assert str(bar.test)=="<function bar.test>"
+assert str(obj.test)=="<bound method foo.test of <object 'foo'>>"
 assert obj.A == 8
 assert obj.x == 0
 assert obj.test()=='test in foo'
 assert obj.test1(2)=='test1test1'
 assert obj.test2()=='test2'
+
+assert obj.machin == 99
 
 print('passed all tests...')
